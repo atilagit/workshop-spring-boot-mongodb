4c450a93626b0fd33693c7816790f953b86438cf
Atila Silva <atila__7@hotmail.com>
2020-08-13 11:16:18 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/atilasilva/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atilasilva.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
